e1bcf136c60da78e1fe5f0fb552ae9b2c157bae4
javi-gv94 <javier.garrayo@hotmail.com>
2019-05-02 16:14:55 +0200

    now working with DREAM data

--- src/app.js
+++ src/app.js
@@ -1,70 +1,103 @@
 import * as d3 from 'd3';
-import * as d3Tip from 'd3-tip';
+import d3Tip from "d3-tip";
 import './app.css';
 
-var margin = {top: 40, right: 20, bottom: 30, left: 40},
+var margin = {top: 40, right: 20, bottom: 60, left: 60},
 width = 960 - margin.left - margin.right,
-height = 500 - margin.top - margin.bottom;
+height = 600 - margin.top - margin.bottom;
 
 var formatPercent = d3.format(".0%");
 
-var x = d3.scaleOrdinal()
-.range([0, width], .1);
+d3.tsv("drug_sensitivity.tsv").then (function ( data) {
 
-var y = d3.scaleLinear()
-.range([height, 0]);
+   // sort data
+  //  data.sort(function(b, a) {
+  //   return a.frequency - b.frequency;
+  // });
 
-var xAxis = d3.axisBottom(x)
-.orient("bottom");
+  let min_y = d3.min(data, function(d) { return d.frequency; });
+  let max_y = d3.max(data, function(d) { return d.frequency; });
 
-var yAxis = d3.axisLeft(y)
-.orient("left")
-.tickFormat(formatPercent);
+  var x = d3.scaleBand()
+  .range([0, width], .1)
+  .domain(data.map(function(d) { return d.letter; }))
+  .padding(0.2);
 
-var tip = d3Tip()
-.attr('class', 'd3-tip')
-.offset([-10, 0])
-.html(function(d) {
-return "<strong>Frequency:</strong> <span style='color:red'>" + d.frequency + "</span>";
-})
+  var y = d3.scaleLinear()
+  .domain([min_y, max_y]).nice()
+  .range([height, 0]);
 
-var svg = d3.select("body").append("svg")
-.attr("width", width + margin.left + margin.right)
-.attr("height", height + margin.top + margin.bottom)
-.append("g")
-.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+ 
+  var xAxis = d3.axisBottom(x);
 
-svg.call(tip);
+  var yAxis = d3.axisLeft(y).ticks(5);
+  // .tickFormat(formatPercent);
 
-d3.tsv("data.tsv", type, function(error, data) {
-x.domain(data.map(function(d) { return d.letter; }));
-y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
+  var tip = d3Tip();
+  tip.attr('class', 'd3-tip')
+  .offset([-10, 0])
+  .html(function(d) {
+  return "<strong>WPC-index:</strong> <span style='color:red'>" + d.frequency + "</span>";
+  })
 
-svg.append("g")
-  .attr("class", "x axis")
-  .attr("transform", "translate(0," + height + ")")
-  .call(xAxis);
+  var svg = d3.select("body").append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-svg.append("g")
-  .attr("class", "y axis")
-  .call(yAxis)
-.append("text")
-  .attr("transform", "rotate(-90)")
-  .attr("y", 6)
-  .attr("dy", ".71em")
-  .style("text-anchor", "end")
-  .text("Frequency");
+  svg.call(tip);
 
-svg.selectAll(".bar")
-  .data(data)
-.enter().append("rect")
-  .attr("class", "bar")
-  .attr("x", function(d) { return x(d.letter); })
-  .attr("width", x.rangeBand())
-  .attr("y", function(d) { return y(d.frequency); })
-  .attr("height", function(d) { return height - y(d.frequency); })
-  .on('mouseover', tip.show)
-  .on('mouseout', tip.hide)
+  //add axis labels
+  svg.append("text")             
+  .attr("transform",
+            "translate(" + (width/2) + " ," + 
+                           (height + margin.top + 10) + ")")
+  .style("text-anchor", "middle")
+  .style("font-weight", "bold")
+  .style("font-size", ".75vw")
+  .text("TOOLS");
+
+  svg.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left)
+      .attr("x",0 - (height / 2))
+      .attr("dy", "1em")
+      .style("text-anchor", "middle")
+      .style("font-weight", "bold")
+      .style("font-size", ".75vw")
+      .text("WPC-index"); 
+
+  svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis)
+    .selectAll("text")
+    .style("text-anchor", "end")
+    .attr("dx", "-.8em")
+    .attr("dy", "-.55em")
+    .attr("transform", "rotate(-90)" );
+
+  svg.append("g")
+    .attr("class", "y axis")
+    .call(yAxis)
+  .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 6)
+    .attr("dy", ".71em")
+    .style("text-anchor", "end")
+    .text("Frequency");
+
+  svg.selectAll(".bar")
+    .data(data)
+    .enter().append("rect")
+    .attr("class", "bar")
+    .attr("x", function(d) { return x(d.letter); })
+    .attr("width", x.bandwidth())
+    .attr("y", function(d) { return y(d.frequency); })
+    .attr("height", function(d) { return height - y(d.frequency); })
+    .on('mouseover', tip.show)
+    .on('mouseout', tip.hide)
 
 });
 
